90911c0d2632f44bc791a90ccc16446eb497658a
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-02-20 20:54:06 +0000

    Import tools sources from the previous SVN repo at revision 3959
    Old repo path: https://sourceforge.net/p/wikindxold/svn/HEAD/tree/
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@4 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ tools/i18n/extract-locales.php
@@ -0,0 +1,159 @@
+<?php
+
+$LanguageCode = ["aa","aar","ab","abk","ace","ach","ada","ady","ae","af","afa","afh","afr","ain","ak","aka","akk","alb","ale","alg","alt","am","amh","an","ang","anp","apa","ar","ara","arc","arg","arm","arn","arp","art","arw","as","asm","ast","ath","aus","av","ava","ave","awa","ay","aym","az","aze","ba","bad","bai","bak","bal","bam","ban","baq","bas","bat","be","bej","bel","bem","ben","ber","bg","bh","bho","bi","bih","bik","bin","bis","bla","bm","bn","bnt","bo","bod","bos","br","bra","bre","bs","btk","bua","bug","bul","bur","byn","ca","cad","cai","car","cat","cau","ce","ceb","cel","ces","ch","cha","chb","che","chg","chi","chk","chm","chn","cho","chp","chr","chu","chv","chy","cmc","cnr","co","cop","cor","cos","cpe","cpf","cpp","cr","cre","crh","crp","cs","csb","cu","cus","cv","cy","cym","cze","da","dak","dan","dar","day","de","del","den","deu","dgr","din","div","doi","dra","dsb","dua","dum","dut","dv","dyu","dz","dzo","ee","efi","egy","eka","el","ell","elx","en","eng","enm","eo","epo","es ","est","et","eu","eus","ewe","ewo","fa","fan","fao","fas","fat","ff","fi","fij","fil","fin","fiu","fj","fo","fon","fr","fra","fre","frm","fro","frr","frs","fry","ful","fur","fy","ga","gaa","gay","gba","gd","gem","geo","ger","gez","gil","gl","gla","gle","glg","glv","gmh","gn","goh","gon","gor","got","grb","grc","gre","grn","gsw","gu","guj","gv","gwi","ha","hai","hat","hau","haw","he","heb","her","hi","hil","him","hin","hit","hmn","hmo","ho","hr","hrv","hsb","ht","hu","hun","hup","hy","hye","hz","ia","iba","ibo","ice","id","ido","ie","ig","ii","iii","ijo","ik","iku","ile","ilo","ina","inc","ind","ine","inh","io","ipk","ira","iro","is","isl","it","ita","iu","ja","jav","jbo","jpn","jpr","jrb","jv","ka","kaa","kab","kac","kal","kam","kan","kar","kas","kat","kau","kaw","kaz","kbd","kg","kha","khi","khm","kho","ki","kik","kin","kir","kj","kk","kl","km","kmb","kn","ko","kok","kom","kon","kor","kos","kpe","kr","krc","krl","kro","kru","ks","ku","kua","kum","kur","kut","kv","kw","ky","la","lad","lah","lam","lao","lat","lav","lb","lez","lg","li","lim","lin","lit","ln","lo","lol","loz","lt","ltz","lu","lua","lub","lug","lui","lun","luo","lus","lv","mac","mad","mag","mah","mai","mak","mal","man","mao","map","mar","mas","may","mdf","mdr","men","mg","mga","mh","mi","mic","min","mis","mk","mkd","mkh","ml","mlg","mlt","mn","mnc","mni","mno","moh","mon","mos","mr","mri","ms","msa","mt","mul","mun","mus","mwl","mwr","my","mya","myn","myv","na","nah","nai","nap","nau","nav","nb","nbl","nd","nde","ndo","nds","ne","nep","new","ng","nia","nic","niu","nl","nld","nn","nno","no","nob","nog","non","nor","nqo","nr","nso","nub","nv","nwc","ny","nya","nym","nyn","nyo","nzi","oc","oci","oj","oji","om","or","ori","orm","os","osa","oss","ota","oto","pa","paa","pag","pal","pam","pan","pap","pau","peo","per","phi","phn","pi","pl","pli","pol","pon","por","pra","pro","ps","pt","pus","qaa-qtz","qu","que","raj","rap","rar","rm","rn","ro","roa","roh","rom","ron","ru","rum","run","rup","rus","rw","sa","sad","sag","sah","sai","sal","sam","san","sas","sat","sc","scn","sco","sd","se","sel","sem","sg","sga","sgn","shn","si","sid","sin","sio","sit","sk","sl","sla","slk","slo","slv","sm","sma","sme","smi","smj","smn","smo","sms","sn","sna","snd","snk","so","sog","som","son","sot","spa","sq","sqi","sr","srd","srn","srp","srr","ss","ssa","ssw","st","su","suk","sun","sus","sux","sv","sw","swa","swe","syc","syr","ta","tah","tai","tam","tat","te","tel","tem","ter","tet","tg","tgk","tgl","th","tha","ti","tib","tig","tir","tiv","tk","tkl","tl","tlh","tli","tmh","tn","to","tog","ton","tpi","tr","ts","tsi","tsn","tso","tt","tuk","tum","tup","tur","tut","tvl","tw","twi","ty","tyv","udm","ug","uga","uig","uk","ukr","umb","und","ur","urd","uz","uzb","vai","ve","ven","vi","vie","vo","vol","vot","wa","wak","wal","war","was","wel","wen","wln","wo","wol","xal","xh","xho","yao","yap","yi ","yid","yo","yor","ypk","za","zap","zbl","zen","zgh","zh","zha","zho","znd","zu","zul","zun","zxx","zza"];
+
+$CountryCode = ["AD","AE","AF","AG","AI","AL","AM","AO","AQ","AR","AS","AT","AU","AW","AX","AZ","BA","BB","BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BQ","BR","BS","BT","BV","BW","BY","BZ","CA","CC","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CU","CV","CW","CX","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","EH","ER","ES","ET","FI","FJ","FK","FM","FO","FR","GA","GB","GD","GE","GF","GG","GH","GI","GL","GM","GN","GP","GQ","GR","GS","GT","GU","GW","GY","HK","HM","HN","HR","HT","HU","ID","IE","IL","IM","IN","IO","IQ","IR","IS","IT","JE","JM","JO","JP","KE","KG","KH","KI","KM","KN","KP","KR","KW","KY","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","ME","MF","MG","MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MV","MW","MX","MY","MZ","NA","NC","NE","NF","NG","NI","NL","NO","NP","NR","NU","NZ","OM","PA","PE","PF","PG","PH","PK","PL","PM","PN","PR","PS","PT","PW","PY","QA","RE","RO","RS","RU","RW","SA","SB","SC","SD","SE","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SR","SS","ST","SV","SX","SY","SZ","TC","TD","TF","TG","TH","TJ","TK","TL","TM","TN","TO","TR","TT","TV","TW","TZ","UA","UG","UM","US","UY","UZ","VA","VC","VE","VG","VI","VN","VU","WF","WS","YE","YT","ZA","ZM","ZW"];
+
+foreach ($LanguageCode as $language)
+{
+    foreach ($CountryCode as $country)
+    {
+        $matrix_locales[] = $language . "_" . $country;
+    }
+}
+
+asort($matrix_locales);
+
+
+$sys_name = str_replace(" ", "_", strtolower(php_uname("s")));
+
+$locale_a_output = array();
+$errorcode = 0;
+exec("locale -a", $locale_a_output, $errorcode);
+asort($locale_a_output);
+
+file_put_contents("locale_a_output_$sys_name.txt", implode("\n", $locale_a_output));
+
+$intl_locales = resourcebundle_locales("");
+asort($intl_locales);
+
+file_put_contents("intl_output_$sys_name.txt", implode("\n", $intl_locales));
+
+$intl_locales = array_merge($intl_locales, $locale_a_output, $matrix_locales);
+asort($intl_locales);
+
+$intl_locales = array_unique($intl_locales);
+asort($intl_locales);
+
+echo count($intl_locales) . "\n";
+
+
+
+foreach ($intl_locales as $k => $locale)
+{
+    $language = locale_get_primary_language($locale);
+    $region = locale_get_region($locale);
+    $script = locale_get_display_script($locale);
+    
+    echo $locale . "\n";
+    
+    // NB: locale_get_primary_language() and locale_get_region() could return an empty string
+    // and not a NULL value on error, contrary to what is documented
+    if (!empty($language) && !empty($region))
+    {
+        //echo $locale . ":" . $language . ":" . $region . ":" . $script . "\n";
+        
+        $utf8Charset = [];
+        foreach ([".utf8", ".UTF8", ".utf-8", ".UTF-8"] as $charset)
+        {
+            if (strpos($locale, $charset) === FALSE)
+            {
+                $utf8Charset[] = $charset;
+            }
+            else
+            {
+                $utf8Charset = [];
+                break;
+            }
+        }
+        $utf8Charset[] = "";
+        
+        
+        // Try the locale inchaged
+        foreach ($utf8Charset as $charset)
+        {
+            $intl_locales_var[] = $locale . $charset;
+        }
+        
+        
+        // Try the locale with _ separator
+        $locale = str_replace("-", "_", $locale);
+        foreach ($utf8Charset as $charset)
+        {
+            $intl_locales_var[] = $locale . $charset;
+        }
+        
+        
+        // Try the locale with - separator
+        $locale = str_replace("_", "-", $locale);
+        foreach ($utf8Charset as $charset)
+        {
+            $intl_locales_var[] = $locale . $charset;
+        }
+        
+        
+        // Try a locale build form ll and CC code without script and with - separator
+        $locale = $language . "-" . $region;
+        foreach ($utf8Charset as $charset)
+        {
+            $intl_locales_var[] = $locale . $charset;
+        }
+        
+        // Try a locale build form ll and CC code without script and with _ separator
+        $locale = $language . "_" . $region;
+        foreach ($utf8Charset as $charset)
+        {
+            $intl_locales_var[] = $locale . $charset;
+        }
+    }
+}
+
+asort($intl_locales);
+$intl_locales_var = array_unique($intl_locales_var);
+asort($intl_locales);
+
+
+foreach ($intl_locales_var as $k => $locale)
+{
+    $utf8Charset = FALSE;
+    foreach ([".utf8", ".UTF8", ".utf-8", ".UTF-8"] as $charset)
+    {
+        if (strpos($locale, $charset) !== FALSE)
+        {
+            $utf8Charset = TRUE;
+            break;
+        }
+    }
+    
+    if ($utf8Charset)
+        $sys_locale = setlocale(LC_ALL, $locale);
+    else
+        $sys_locale = setlocale(LC_ALL, $locale . ".utf8", $locale . ".UTF8", $locale . ".utf-8", $locale . ".UTF-8", $locale);
+        
+    if ($sys_locale !== FALSE)
+    {
+        echo $locale . ":" . print_r($sys_locale, TRUE) . "\n";
+        $loc_final[] = $sys_locale;
+    }
+    else
+    {
+        echo $locale . ":" . print_r($sys_locale, TRUE) . "\n";
+        $loc_error[] = $locale;
+    }
+}
+
+$loc_final = array_unique($loc_final);
+$loc_error = array_unique($loc_error);
+
+asort($loc_error);
+asort($intl_locales);
+
+echo count($loc_final) . "\n";
+
+file_put_contents("loc_match_$sys_name.txt", implode("\n", $loc_final));
+file_put_contents("loc_error_$sys_name.txt", implode("\n", $loc_error));
+
+echo php_uname("a") . "\n";
+echo php_uname("s") . "\n";
+
+echo "\nEND\n";

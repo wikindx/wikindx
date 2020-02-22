a2cebfc66c91685e8ef7a35bec8b5f7f5d54f93f
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-02-22 18:23:22 +0000

    Fix the default email backend
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@38 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/dbschema/update/12/01_convert_default_email_backend.sql
+++ trunk/dbschema/update/12/01_convert_default_email_backend.sql
@@ -4,10 +4,10 @@
 -- @author The WIKINDX Team
 -- @license https://creativecommons.org/licenses/by-nc-sa/4.0/ CC-BY-NC-SA 4.0
 -- 
--- Convert the backend to the new default when it is "mail", which is retired
+-- If configMailBackend is not set to an authorized value replace it by the new default value (sendmail).
 
 UPDATE %%WIKINDX_DB_TABLEPREFIX%%config
 SET configVarchar = 'sendmail'
 WHERE
 	configName = 'configMailBackend'
-	AND configVarchar = 'mail';
+	AND configVarchar NOT IN ('sendmail', 'smtp');

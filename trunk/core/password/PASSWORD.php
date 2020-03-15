350aef98fb473130cb2bccb6f1edb5ac8dc9d1c7
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-03-15 23:32:26 +0000

    Unbreak superadmin creation
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@274 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/password/PASSWORD.php
+++ trunk/core/password/PASSWORD.php
@@ -18,12 +18,12 @@ class PASSWORD
     /**
      * Create and return an array of two strings for the username/password form
      *
-     * @param bool|string $username TRUE (default) if username should be part of the form
+     * @param bool|string $usersUsername TRUE (default) if usersUsername should be part of the form
      * @param bool|string $super FALSE (default) if this is for superadmin in the configure form
      *
      * @return array Array of 2 strings: 1. form element string; 2. javascript for form header
      */
-    public function createElements($username = TRUE, $super = FALSE)
+    public function createElements($usersUsername = TRUE, $super = FALSE)
     {
         $errors = FACTORY_ERRORS::getInstance();
         $messages = FACTORY_MESSAGES::getInstance();
@@ -65,10 +65,10 @@ class PASSWORD
         );
         $formText = '';
         if (!$super) {
-            if ($username) {
+            if ($usersUsername) {
                 $formText .= \HTML\td(\FORM\textInput(
                     $messages->text("user", "username"),
-                    "username",
+                    "usersUsername",
                     FALSE,
                     20,
                     255
@@ -92,8 +92,8 @@ class PASSWORD
             // superadmin from Configure menu
             $formText .= \HTML\td(\FORM\textInput(
                 $messages->text("config", "superUsername"),
-                "username",
-                $username,
+                "usersUsername",
+                $usersUsername,
                 20
             ) . " " . \HTML\span('*', 'required'));
             $formText .= \HTML\td(\FORM\passwordInput(

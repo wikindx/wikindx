387a14d7130bf5b93e5a99f7c8316e33f8559421
sirfragalot <sirfragalot@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-06-01 08:59:18 +0000

    Add a new plugin: ideagen Idea Generator randomly selects and displays two items of metadata (quotes, paraphrases, musings, or ideas) in the hope that the chance juxtaposition might be serendipitous. If so, then a new idea can be stored.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@374 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ trunk/components/plugins/ideagen/ideagenMessages.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WIKINDX : Bibliographic Management system.
+ *
+ * @see https://wikindx.sourceforge.io/ The WIKINDX SourceForge project
+ *
+ * @author The WIKINDX Team
+ * @license https://creativecommons.org/licenses/by-nc-sa/4.0/ CC-BY-NC-SA 4.0
+ */
+class ideagenMessages
+{
+    /** array */
+    public $text = [];
+
+    public function __construct()
+    {
+        $domain = mb_strtolower(basename(__DIR__));
+        
+        $this->text = [
+            "menu" => dgettext($domain, "IdeaGen"),
+            "heading" => dgettext($domain, "Idea Generator"),
+            "generate" => dgettext($domain, "Generate"),
+            "generateAgain" => dgettext($domain, "Generate again"),
+            "description" => dgettext($domain, "Idea Generator randomly selects and displays two items of metadata (quotes, paraphrases, musings, or ideas) in the hope that the chance juxtaposition might be serendipitous."),
+            "newIdea" => dgettext($domain, "Create new idea"),
+            "idea" => dgettext($domain, "Idea"),
+            "musing" => dgettext($domain, "Musing"),
+            "paraphrase" => dgettext($domain, "Paraphrase"),
+            "quote" => dgettext($domain, "Quote"),
+            "addIdea" => dgettext($domain, "Add a new idea"),
+        ];
+    }
+}

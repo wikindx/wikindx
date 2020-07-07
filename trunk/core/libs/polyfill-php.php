52adf10524321b4d8a926227b815d1782b570e0f
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-07-07 22:11:39 +0000

    Fix PHP polyfills for PHP < 7.2
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@459 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/libs/polyfill-php.php
+++ trunk/core/libs/polyfill-php.php
@@ -238,50 +238,53 @@ if (PHP_VERSION_ID < 70200) {
 	define('PHP_FLOAT_MIN', 2.2250738585072E-308);
 	define('PHP_FLOAT_MAX', 1.7976931348623157E+308);
 	
-	function utf8_encode($s)
+	if (!extension_loaded("libxml"))
 	{
-	    $s .= $s;
-	    $len = \strlen($s);
-	
-	    for ($i = $len >> 1, $j = 0; $i < $len; ++$i, ++$j) {
-	        switch (true) {
-	            case $s[$i] < "\x80": $s[$j] = $s[$i]; break;
-	            case $s[$i] < "\xC0": $s[$j] = "\xC2"; $s[++$j] = $s[$i]; break;
-	            default: $s[$j] = "\xC3"; $s[++$j] = \chr(\ord($s[$i]) - 64); break;
-	        }
-	    }
-	
-	    return substr($s, 0, $j);
-	}
-	
-	function utf8_decode($s)
-	{
-	    $s = (string) $s;
-	    $len = \strlen($s);
-	
-	    for ($i = 0, $j = 0; $i < $len; ++$i, ++$j) {
-	        switch ($s[$i] & "\xF0") {
-	            case "\xC0":
-	            case "\xD0":
-	                $c = (\ord($s[$i] & "\x1F") << 6) | \ord($s[++$i] & "\x3F");
-	                $s[$j] = $c < 256 ? \chr($c) : '?';
-	                break;
-	
-	            case "\xF0":
-	                ++$i;
-	                // no break
-	
-	            case "\xE0":
-	                $s[$j] = '?';
-	                $i += 2;
-	                break;
-	
-	            default:
-	                $s[$j] = $s[$i];
-	        }
-	    }
-	
-	    return substr($s, 0, $j);
+    	function utf8_encode($s)
+    	{
+    	    $s .= $s;
+    	    $len = \strlen($s);
+    	
+    	    for ($i = $len >> 1, $j = 0; $i < $len; ++$i, ++$j) {
+    	        switch (true) {
+    	            case $s[$i] < "\x80": $s[$j] = $s[$i]; break;
+    	            case $s[$i] < "\xC0": $s[$j] = "\xC2"; $s[++$j] = $s[$i]; break;
+    	            default: $s[$j] = "\xC3"; $s[++$j] = \chr(\ord($s[$i]) - 64); break;
+    	        }
+    	    }
+    	
+    	    return substr($s, 0, $j);
+    	}
+    	
+    	function utf8_decode($s)
+    	{
+    	    $s = (string) $s;
+    	    $len = \strlen($s);
+    	
+    	    for ($i = 0, $j = 0; $i < $len; ++$i, ++$j) {
+    	        switch ($s[$i] & "\xF0") {
+    	            case "\xC0":
+    	            case "\xD0":
+    	                $c = (\ord($s[$i] & "\x1F") << 6) | \ord($s[++$i] & "\x3F");
+    	                $s[$j] = $c < 256 ? \chr($c) : '?';
+    	                break;
+    	
+    	            case "\xF0":
+    	                ++$i;
+    	                // no break
+    	
+    	            case "\xE0":
+    	                $s[$j] = '?';
+    	                $i += 2;
+    	                break;
+    	
+    	            default:
+    	                $s[$j] = $s[$i];
+    	        }
+    	    }
+    	
+    	    return substr($s, 0, $j);
+    	}
 	}
 	
 	function php_os_family()

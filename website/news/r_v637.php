990d572e6656a767a1bf1bf3feaa40856c1981ae
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-07-21 20:33:56 +0000

    Release 6.3.7 (cus)
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@533 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ website/news/r_v637.php
@@ -0,0 +1,17 @@
+<h2>Wikindx v6.3.7</h2>
+<div class="hDetails">July, 2020</div>
+<h3>CHANGELOG</h3>
+
+<p><strong>Minor bug fixes and feature enhancements.</strong></p>
+
+<h3>BUGS:</h3>
+
+<ul>
+    <li>Correct a path for a file icon.</li>
+</ul>
+
+<h3>FEATURE ENHANCEMENTS:</h3>
+
+<ul>
+    <li>Added delete icons to individual quotes, paraphrases, and musings when viewing a resource.</li>
+</ul>

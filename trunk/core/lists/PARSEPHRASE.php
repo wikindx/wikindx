6cf33462d022e94def8749e3c6a87f57ab0ffe25
sirfragalot <sirfragalot@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-08-12 13:33:08 +0000

    Expand use of array_filter() in PARSEPHRASE for ideas.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@564 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/lists/PARSEPHRASE.php
+++ trunk/core/lists/PARSEPHRASE.php
@@ -261,11 +261,11 @@ class PARSEPHRASE
             $this->orsFT[] = ltrim(array_pop($this->andsFT), '+');
         }
         if (!$this->idea) {
-            $storedHighlight = UTF8::mb_explode(',', $this->session->getVar("search_Highlight"));
+            $storedHighlight = array_filter(UTF8::mb_explode(',', $this->session->getVar("search_Highlight")));
             $searchHighlight = array_unique(array_merge($storedHighlight, $searchHighlight));
             $this->session->setVar("search_Highlight", implode(",", $searchHighlight));
         } else {
-            $storedHighlight = UTF8::mb_explode(',', $this->session->getVar("search_HighlightIdea"));
+            $storedHighlight = array_filter(UTF8::mb_explode(',', $this->session->getVar("search_HighlightIdea")));
             $searchHighlight = array_unique(array_merge($storedHighlight, $searchHighlight));
             $this->session->setVar("search_HighlightIdea", implode(",", $searchHighlight));
         }

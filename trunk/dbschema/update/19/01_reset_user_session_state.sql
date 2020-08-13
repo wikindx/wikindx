3b7ca0b63c7e0a5eb49f6acf2ac2e6ed1a2bead0
sirfragalot <sirfragalot@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-08-13 09:42:47 +0000

    Add missing SQL files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@574 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ trunk/dbschema/update/19/01_reset_user_session_state.sql
@@ -0,0 +1,10 @@
+-- 
+-- WIKINDX : Bibliographic Management system.
+-- @link https://wikindx.sourceforge.io/ The WIKINDX SourceForge project
+-- @author The WIKINDX Team
+-- @license https://www.isc.org/licenses/ ISC License
+--
+-- Following change to storage of session arrays, reset the session state variable to NULL
+
+UPDATE %%WIKINDX_DB_TABLEPREFIX%%users
+SET usersUserSession = NULL

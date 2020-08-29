ef56985f80868b5679c285f322b7d225ad990aaa
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-08-29 16:10:27 +0000

    6.3.9 release (website)
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@624 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ website/news/p_v639.php
@@ -0,0 +1,33 @@
+<h2>Wikindx v6.3.9</h2>
+<div class="hDetails">August, 2020</div>
+<h3>CHANGELOG</h3>
+
+<p><strong>Minor bug fixes, maintenance, and feature enhancements.</strong></p>
+
+<p><strong>NB: Intl PHP extension is mandatory now.</strong></p>
+
+<h3>BUGS:</h3>
+
+<ul>
+    <li>When viewing a list, fix the use of 'Use all checked' and 'Use all in list' when organizing resources, adding to basket etc.</li>
+    <li>Add back in the missing Admin|Languages menu item.</li>
+    <li>Fix global deletion when editing note and note happens to be an empty string.</li>
+    <li>Fix paging of ideas from Advanced Search.</li>
+    <li>Ensure that an idea can comprise just an image.</li>
+    <li>Fixed case where noteEdit to second resource are lost upon saving after noteEdit of first resource are saved (editing resources in multiple tabs).</li>
+    <li>Extended fix #6 to also allow editing individual quotes, paraphrases, and musing independently of other edits.</li>
+</ul>
+
+<h3>FEATURE ENHANCEMENTS:</h3>
+
+<ul>
+    <li>Update Smarty (v3.1.36)</li>
+    <li>Intl PHP extension is mandatory now.</li>
+</ul>
+
+<h3>FEATURE ENHANCEMENTS:</h3>
+
+<ul>
+    <li>Improved the interface for managing uploaded images (Admin|Images) by displaying thumbnails of images selected for deletion. When deleting images, replace any reference to them in metadata and front page description with "[Image deleted by WIKINDX Administrator]."</li>
+    <li>Improved the interface for grouping and ungrouping creators (Admin|Creators...|Group).</li>
+</ul>

2266615e686f370ae046df683416ef448f4cf95d
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-11-15 01:03:32 +0000

    Remove dead code
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@918 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/tiny_mce/LOADTINYMCE.php
+++ trunk/core/tiny_mce/LOADTINYMCE.php
@@ -38,57 +38,6 @@ class LOADTINYMCE
         $this->cssPopupPath = WIKINDX_URL_BASE . '/' . WIKINDX_URL_COMPONENT_TEMPLATES . '/' . GLOBALS::getUserVar('Template') . '/template.css?ver=' . WIKINDX_PUBLIC_VERSION;
     }
     /**
-     * Load tinymce -- full configuration
-     *
-     * @return string
-     */
-    public function loadFull()
-    {
-        $cssPath = $this->cssPath;
-        $cssPopupPath = $this->cssPopupPath;
-        $tinymcePath = $this->getIncludeTinyMceLib();
-
-        return <<<END
-$tinymcePath
-<script>
-tinyMCE.init({
-        // General options
-        mode : "textareas",
-		editor_deselector : "mceNoEditor",
-        theme : "advanced",
-        element_format : "html",
-        // Keep as UTF8
-        entity_encoding : "raw",
-        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,wikindxContextMenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,wikindxCite,wikindxSpecialChars",
-
-        // Theme options
-        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,removeformat,code,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-        theme_advanced_buttons3 : "tablecontrols,|,hr,visualaid,|,sub,sup,|,wikindxSpecialChars,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,wikindxCite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_resizing : true,
-        theme_advanced_statusbar_location : "none",
-
-        // Skin options
-        skin : "o2k7",
-        skin_variant : "silver",
-
-        // Example content CSS (should be your site CSS)
-        content_css : "$cssPath",
-        popup_css : "$cssPopupPath",
-
-        // Drop lists for link/image/media/template dialogs
-        template_external_list_url : "js/template_list.js",
-        external_link_list_url : "js/link_list.js",
-        external_image_list_url : "js/image_list.js",
-        media_external_list_url : "js/media_list.js"
-});
-</script>
-END;
-    }
-    /**
      * Load tinymce -- metadata textarea configuration
      *
      * @param array $elements Array of textinput ids to apply editor to. Default is array()

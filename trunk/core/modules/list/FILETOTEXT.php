d56bc8b632406f73b60a8d9d42b64ca6167fb865
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-11-24 01:48:45 +0000

    Use the same semantics for all text extraction functions and use the timeout functionality of the PdfToText class
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@949 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/modules/list/FILETOTEXT.php
+++ trunk/core/modules/list/FILETOTEXT.php
@@ -19,10 +19,6 @@
 class FILETOTEXT
 {
     /** string */
-    public $decodedtext = '';
-    /** array */
-    public $readFiles = [];
-    /** string */
     private $fileName;
 
     public function __construct()
@@ -241,129 +237,148 @@ class FILETOTEXT
         $finfo = new \finfo(FILEINFO_MIME_TYPE);
         $mimeType = $finfo->file($filename);
         
-        $this->fileName = $filename;
-        if (isset($this->fileName) && !file_exists($this->fileName))
-        {
-            return FALSE;
+        // Convert to text with a specific function by mimetype and return it
+        switch ($mimeType) {
+            case WIKINDX_MIMETYPE_DOC:
+                $text = $this->readWord($filename);
+            break;
+            case WIKINDX_MIMETYPE_DOCX:
+                $text = $this->readDocx($filename);
+            break;
+            case WIKINDX_MIMETYPE_PDF:
+                $text = $this->readPdf($filename);
+            break;
+            case WIKINDX_MIMETYPE_TXT:
+                $text = $this->readText($filename);
+            break;
+            default:
+                // Type not handled
+                $text = "";
+            break;
         }
-        if (array_key_exists($this->fileName, $this->readFiles))
+        
+        // If the content returned is not a string, drop it and return an empty text
+        if (!is_string($text))
         {
-            return $this->readFiles[$this->fileName];
+            $text = "";
         }
-        if ($mimeType == WIKINDX_MIMETYPE_TXT)
-        {
-            $text = $this->readText();
-        }
-        elseif ($mimeType == WIKINDX_MIMETYPE_DOC)
-        {
-            $text = $this->readWord();
-        }
-        elseif ($mimeType == WIKINDX_MIMETYPE_DOCX)
-        {
-            $text = $this->read_docx();
-        }
-        elseif ($mimeType == WIKINDX_MIMETYPE_PDF)
-        {
-            ini_set('memory_limit', '-1'); // PDF objects can be large – memory is reset at the next script
-            $importPDF = new PdfToText($this->fileName, PdfToText::PDFOPT_NO_HYPHENATED_WORDS);
-            if ($importPDF->Text)
-            {
-                $this->readFiles[$this->fileName] = $importPDF->Text;
-
-                return $importPDF->Text;
-            }
-            $text = FALSE;
-        }
-        if ($text)
-        {
-            $this->readFiles[$this->fileName] = $text;
-
-            return $text;
-        }
-        else
-        {
-            return FALSE;
-        }
+        
+        return $text;
     }
+    /**
+     * readPdf
+     *
+     * @param mixed $filename
+     *
+     * @return string
+     */
+    private function readPdf($filename)
+    {
+        // PDF objects can be large – memory is reset at the next script
+        ini_set('memory_limit', '-1');
+        $importPDF = new PdfToText();
+        
+        // Note:
+        // MaxGlobalExecutionTime property and PDFOPT_ENFORCE_GLOBAL_EXECUTION_TIME option are broken
+        // use only one instance of the class by file parsed
+        
+        // PDFOPT_NO_HYPHENATED_WORDS: tries to join back hyphenated words into a single word
+        // PDFOPT_ENFORCE_EXECUTION_TIME: throw a PdfToTextTimeout exception if the extraction run more than MaxExecutionTime
+        $importPDF->Options = PdfToText::PDFOPT_NO_HYPHENATED_WORDS | PdfToText::PDFOPT_ENFORCE_EXECUTION_TIME;
+        
+        // Will consume all available runtime except 2 seconds (if this point is reached in less than 2 seconds)
+        $importPDF->MaxExecutionTime = ini_get('max_execution_time') - GLOBALS::getPageElapsedTime() - 2;
+        
+        
+        try
+        {
+            // Return the text extracted
+            $text = $importPDF->Load($filename);
+        } catch (
+            // or catch all PdfToText exceptions and return an empty string
+            PdfToTextException
+            | PdfToTextCaptureException
+            | PdfToTextDecodingException
+            | PdfToTextDecryptionException
+            | PdfToTextFormException
+            | PdfToTextTimeoutException $e
+        ) {
+            $text = "PdfToTextTimeoutException " . $e->getMessage();
+        }
+        
+        return $text;
+    }
+    
     /**
      * readText
      *
-     * @return false|string
+     * @param mixed $filename
+     *
+     * @return string
      */
-    private function readText()
+    private function readText($filename)
     {
-        if (file_exists($this->fileName))
-        {
-            if (($text = file_get_contents($this->fileName)) !== FALSE)
-            {
-                return $text;
-            }
-            else
-            {
-                return FALSE;
-            }
-        }
+        $content = file_get_contents($filename);
+        if ($content === FALSE)
+            return "";
         else
-        {
-            return FALSE;
-        }
+            return $content;
     }
+    
     /**
      * readWord
      *
-     * @return false|string
+     * @param string $filename
+     *
+     * @return string
      */
-    private function readWord()
+    private function readWord($filename)
     {
-        if (file_exists($this->fileName))
+        if (($fh = fopen($filename, 'r')) !== FALSE)
         {
-            if (($fh = fopen($this->fileName, 'r')) !== FALSE)
+            $headers = fread($fh, 0xA00);
+
+            // 1 = (ord(n)*1) ; Document has from 0 to 255 characters
+            $n1 = (ord($headers[0x21C]) - 1);
+
+            // 1 = ((ord(n)-8)*256) ; Document has from 256 to 63743 characters
+            $n2 = ((ord($headers[0x21D]) - 8) * 256);
+
+            // 1 = ((ord(n)*256)*256) ; Document has from 63744 to 16775423 characters
+            $n3 = ((ord($headers[0x21E]) * 256) * 256);
+
+            // 1 = (((ord(n)*256)*256)*256) ; Document has from 16775424 to 4294965504 characters
+            $n4 = (((ord($headers[0x21F]) * 256) * 256) * 256);
+
+            // Total length of text in the document
+            $textLength = ($n1 + $n2 + $n3 + $n4);
+            if ($textLength <= 0)
             {
-                $headers = fread($fh, 0xA00);
-
-                // 1 = (ord(n)*1) ; Document has from 0 to 255 characters
-                $n1 = (ord($headers[0x21C]) - 1);
-
-                // 1 = ((ord(n)-8)*256) ; Document has from 256 to 63743 characters
-                $n2 = ((ord($headers[0x21D]) - 8) * 256);
-
-                // 1 = ((ord(n)*256)*256) ; Document has from 63744 to 16775423 characters
-                $n3 = ((ord($headers[0x21E]) * 256) * 256);
-
-                // 1 = (((ord(n)*256)*256)*256) ; Document has from 16775424 to 4294965504 characters
-                $n4 = (((ord($headers[0x21F]) * 256) * 256) * 256);
-
-                // Total length of text in the document
-                $textLength = ($n1 + $n2 + $n3 + $n4);
-                if ($textLength <= 0)
-                {
-                    return FALSE;
-                }
-                $extracted_plaintext = fread($fh, $textLength);
-                fclose($fh);
-
-                return utf8_encode($extracted_plaintext);
+                return "";
             }
-            else
-            {
-                return FALSE;
-            }
+            $extracted_plaintext = fread($fh, $textLength);
+            fclose($fh);
+
+            return utf8_encode($extracted_plaintext);
         }
         else
         {
-            return FALSE;
+            return "";
         }
     }
+    
     /**
-     * read_docx
+     * readDocx
      *
-     * @return false|string
+     * @param string $filename
+     *
+     * @return string
      */
-    private function read_docx()
+    private function readDocx($filename)
     {
         $striped_content = '';
         $content = '';
-        $zip = zip_open($this->fileName);
+        $zip = zip_open($filename);
         if (!$zip || is_numeric($zip))
         {
             return FALSE;

903cb8d903ec13a5ef569ac264b20baf6d0c9ec5
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-12-07 14:35:37 +0000

    Uniform license
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1012 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/cli-make-languages.php
+++ trunk/cli-make-languages.php
@@ -1,21 +1,12 @@
 <?php
-/*
-ISC License
-
-Copyright (c) 2019, Stéphane Aulery, <lkppo@users.sourceforge.net>
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
+/**
+ * WIKINDX : Bibliographic Management system.
+ *
+ * @see https://wikindx.sourceforge.io/ The WIKINDX SourceForge project
+ *
+ * @author The WIKINDX Team
+ * @license https://www.isc.org/licenses/ ISC License
+ */
 
 include_once("core/startup/CONSTANTS.php");
 include_once("core/libs/FILE.php");

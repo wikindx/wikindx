b69e8a86209a739019e7d3586b76c1d11ed11d27
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-12-17 02:22:55 +0000

    Migrate option configEmailnewRegistration to configEmailnewRegistration with several versions of delay
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1062 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ trunk/dbschema/update/36/02_restore_configEmailnewRegistration.sql
@@ -0,0 +1,19 @@
+-- 
+-- WIKINDX : Bibliographic Management system.
+-- @link https://wikindx.sourceforge.io/ The WIKINDX SourceForge project
+-- @author The WIKINDX Team
+-- @license https://www.isc.org/licenses/ ISC License
+--
+
+-- Copy the value of option configEmailnewRegistration to configEmailnewRegistrations
+-- On some installations the option name is without the S, probably due to a bug that went unnoticed.
+-- We try to retrieve the value of the old option in the new one if the value still exists.
+UPDATE %%WIKINDX_DB_TABLEPREFIX%%config AS t1
+    INNER JOIN %%WIKINDX_DB_TABLEPREFIX%%config AS t2
+        ON t1.configName = 'configEmailnewRegistration'
+            AND t2.configName = 'configEmailnewRegistrations'
+SET t2.configVarchar = t1.configVarchar;
+
+-- Remove configEmailnewRegistration option
+DELETE FROM %%WIKINDX_DB_TABLEPREFIX%%config
+WHERE configName = 'configEmailnewRegistration';

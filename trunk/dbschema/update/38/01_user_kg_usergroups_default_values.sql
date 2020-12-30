37bf0056a04110fcab529980925f8a8d254ee6d5
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2020-12-30 11:45:39 +0000

    Fix the wrong definition of user_kg_usergroups.userkgusergroupsUserGroupId (NULL is allowed)
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1125 b5da76e6-9d34-46e5-a24a-8866c81a9b49

---
+++ trunk/dbschema/update/38/01_user_kg_usergroups_default_values.sql
@@ -0,0 +1,10 @@
+-- 
+-- WIKINDX : Bibliographic Management system.
+-- @link https://wikindx.sourceforge.io/ The WIKINDX SourceForge project
+-- @author The WIKINDX Team
+-- @license https://www.isc.org/licenses/ ISC License
+--
+-- Correct default value for user_kg_usergroups table
+-- Fix the wrong definition of this field in 6.4.0 (36) for a fisrt installation
+
+ALTER TABLE %%WIKINDX_DB_TABLEPREFIX%%user_kg_usergroups MODIFY COLUMN `userkgusergroupsUserGroupId` int(11) DEFAULT NULL;

ad21cf84ed3739e8732d370d59cf94f04330f775
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2021-02-03 21:01:20 +0000

    Delete the files of the old website
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1275 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- website/404/index.php
+++
@@ -1,3 +0,0 @@
-<h1>404 Error</h1>
-
-<p>Sorry, the page requested is no longer available or has been moved.</p>
--- website/users/index.php
+++
@@ -1,7 +0,0 @@
-WIKINDX is widely used around the world including at NASA and a range of universities and research institutions. Some WIKINDX users:
-<ul>
-    <li><a href='http://wikindx.inrp.fr/biblio_vst/index.php' target='_blank'>Institut National de Recherche Pédagogique,  Bibliographies du service Veille et Analyses</a></li>
-    <li><a href='http://www.bibliographia.net/wikindx3/index.php' target='_blank'>University Theses in Russian, Soviet and East European Studies 1907-2011</a></li>
-    <li><a href='http://www.comicforschung.uni-bonn.de/index.php' target='_blank'>University of Bonn, Online-Bibliographie zur Comicforschung</a></li>
-    <li><a href='https://www.ttcn.de//bibliography/' target='_blank'>The Fraunhofer Institute, TTCN-3</a></li>
-</ul>

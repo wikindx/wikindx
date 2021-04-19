9612158e4fd989638da247ca02eae6f893671cec
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2021-04-19 11:57:54 +0000

    6.4.7 release date
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1669 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/startup/CONSTANTS.php
+++ trunk/core/startup/CONSTANTS.php
@@ -112,7 +112,7 @@ define('WIKINDX_COPYRIGHT_YEAR', "2003-2021");
  *
  * @name WIKINDX_RELEASE_DATE
  */
-define('WIKINDX_RELEASE_DATE', "2021-04-10");
+define('WIKINDX_RELEASE_DATE', "2021-04-19");
 /**
  * WIKINDX release timestamp
  *

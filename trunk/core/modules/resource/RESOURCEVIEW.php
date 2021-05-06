84b4b40b0f9e84fa3d7380f8e1a8cadbaacfc0fb
sirfragalot <sirfragalot@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2021-05-06 13:32:15 +0000

    Fix a bug in resource view for the delete icon for a user's own resource if they are not the superadmin.
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1790 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/core/modules/resource/RESOURCEVIEW.php
+++ trunk/core/modules/resource/RESOURCEVIEW.php
@@ -1027,8 +1027,7 @@ class RESOURCEVIEW
                 $links['delete'] =
                     "index.php?action=admin_DELETERESOURCE_CORE" . htmlentities('&function=deleteResourceConfirm') .
                     htmlentities('&navigate=front&resource_id=' . $row['resourceId']. "&browserTabID=" . $this->browserTabID);
-// Delete? Seems not to be necessary
-//                $links['delete'] = \HTML\a($this->icons->getClass("delete"), $this->icons->getHTML("delete"), $links['delete']);
+                $links['delete'] = \HTML\a($this->icons->getClass("delete"), $this->icons->getHTML("delete"), $links['delete']);
             }
             $edit = $this->allowEdit = TRUE;
         }

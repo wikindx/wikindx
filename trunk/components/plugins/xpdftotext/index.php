6bf1317ef34a6721f590aa92021de1c45f583e6b
lkppo <lkppo@b5da76e6-9d34-46e5-a24a-8866c81a9b49>
2021-05-18 01:34:54 +0000

    Form of xpdftotext plugin [#370]
    git-svn-id: svn+ssh://svn.code.sf.net/p/wikindx/svn@1847 b5da76e6-9d34-46e5-a24a-8866c81a9b49

--- trunk/components/plugins/xpdftotext/index.php
+++ trunk/components/plugins/xpdftotext/index.php
@@ -22,6 +22,9 @@ include_once(implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "..", "..", "core", "s
 
 class xpdftotext_MODULE
 {
+    // Cache directory of this plugin
+    const CACHE_DIRECTORY = WIKINDX_DIR_BASE . DIRECTORY_SEPARATOR . WIKINDX_DIR_CACHE_PLUGINS . DIRECTORY_SEPARATOR . "xpdftotext";
+    
     public $authorize;
     public $menus;
     
@@ -60,6 +63,7 @@ class xpdftotext_MODULE
         include_once(implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "..", "..", "core", "messages", "PLUGINMESSAGES.php"]));
         $this->pluginmessages = new PLUGINMESSAGES('xpdftotext', 'xpdftotextMessages');
         $this->coremessages = FACTORY_MESSAGES::getInstance();
+        $this->coreerrors = FACTORY_ERRORS::getInstance();
         include_once(implode(DIRECTORY_SEPARATOR, [__DIR__, "config.php"]));
         $this->config = new xpdftotext_CONFIG();
         $this->authorize = $this->config->authorize;
@@ -110,14 +114,13 @@ class xpdftotext_MODULE
         {
             if (!is_executable($path))
             {
-                die("
-                <p>Some binaries of XpdftoText plugin need the executable bit.
-                Please add it with chmod. The following commands should do the trick:</p>
-                <ul>
-                    <li>" . htmlspecialchars('chmod +x "' . realpath($pathpdftotext) . '"') . "</li>
-                    <li>" . htmlspecialchars('chmod +x "' . realpath($pathpdfinfo) . '"') . "</li>
-                </ul>
-                ");
+                die(
+                    \HTML\p($this->pluginmessages->text("chmodInvit")) .
+                    "<ul>
+                        <li>" . htmlspecialchars('chmod +x "' . realpath($pathpdftotext) . '"') . "</li>
+                        <li>" . htmlspecialchars('chmod +x "' . realpath($pathpdfinfo) . '"')   . "</li>
+                    </ul>"
+                );
             }
         }
     }
@@ -137,16 +140,236 @@ class xpdftotext_MODULE
      */
     public function init()
     {
-        return $this->display();
+        return $this->display("");
     }
     
     /**
      * display
      *
      * @param mixed $message
+     * @param string $fileNametxt
      */
-    public function display()
+    public function display($message = FALSE, $fileNametxt = "")
     {
-        GLOBALS::addTplVar('content', "");
+        $pString = "";
+        $pString .= $message ?? "";
+        $pString .= \HTML\p($this->pluginmessages->text("introduction"));
+        
+        if (file_exists($fileNametxt))
+        {
+            $pString .= \HTML\hr();
+            $pString .= \HTML\h($this->pluginmessages->text("convertedFileHeading"));
+            
+            $pString .= \HTML\p(\HTML\a("link", basename($fileNametxt), "index.php?action=xpdftotext_downloadFile&amp;filename=" . htmlentities(basename($fileNametxt)), "_blank"));
+        }
+        
+        $pString .= \HTML\hr();
+        $pString .= \HTML\h($this->pluginmessages->text("submissionFormHeading"));
+        
+        if (ini_get("file_uploads"))
+        {
+            $pString .= \FORM\formMultiHeader("xpdftotext_convert");
+            $pString .= \HTML\tableStart('widthauto');
+            $pString .= \HTML\tbodyStart();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("file"));
+            $pString .= \HTML\td(
+                \FORM\fileUpload(
+                    "",
+                    "pdf_File",
+                    30,
+                    ".pdf"
+                )
+                . " (max.&nbsp;" . \FILE\formatSize(\FILE\fileUploadMaxSize()) . ")"
+            );
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("firstPage"));
+            $pString .= \HTML\td(\FORM\textInput("", "f", "", 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("lastPage"));
+            $pString .= \HTML\td(\FORM\textInput("", "l", "", 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("mode"));
+            $pString .= \HTML\td(\FORM\selectedBox("", "mode", ["default", "layout", "lineprinter", "raw", "simple", "simple2", "table"], "default", 1));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("characterPitch"));
+            $pString .= \HTML\td(\FORM\textInput("", "fixed", "", 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("lineSpacing"));
+            $pString .= \HTML\td(\FORM\textInput("", "linespacing", "", 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("clip"));
+            $pString .= \HTML\td(\FORM\checkbox("", "clip", FALSE));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("nodiag"));
+            $pString .= \HTML\td(\FORM\checkbox("", "nodiag", FALSE));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("nopgbrk"));
+            $pString .= \HTML\td(\FORM\checkbox("", "nopgbrk", FALSE));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("marginl"));
+            $pString .= \HTML\td(\FORM\textInput("", "marginl", 0, 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("marginr"));
+            $pString .= \HTML\td(\FORM\textInput("", "marginr", 0, 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("margint"));
+            $pString .= \HTML\td(\FORM\textInput("", "margint", 0, 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("marginb"));
+            $pString .= \HTML\td(\FORM\textInput("", "marginb", 0, 4));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("opw"));
+            $pString .= \HTML\td(\FORM\textInput("", "opw", ""));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\trStart();
+            $pString .= \HTML\td($this->pluginmessages->text("upw"));
+            $pString .= \HTML\td(\FORM\textInput("", "upw", ""));
+            $pString .= \HTML\trEnd();
+            $pString .= \HTML\tbodyEnd();
+            $pString .= \HTML\tableEnd();
+            $pString .= \HTML\p(\FORM\formSubmit($this->coremessages->text("submit", "Submit")));
+        }
+        else
+        {
+            $pString .= \HTML\td($this->coremessages->text("misc", "uploadDisabled"));
+        }
+        
+        foreach (\FILE\fileInDirToArray(self::CACHE_DIRECTORY) as $file)
+        {
+            $path = implode(DIRECTORY_SEPARATOR, [self::CACHE_DIRECTORY, $file]);
+            // Delete outdated files (at least 5 minutes old)
+            if ((time() - filemtime($path)) >= 60 * 5)
+            {
+                @unlink($path);
+            }
+        }
+        
+        GLOBALS::addTplVar('content', $pString);
+    }
+    
+    /**
+     * Convert a PDF to text
+     *
+     * @param mixed $message
+     */
+    public function convert()
+    {
+        $vars = GLOBALS::getVars();
+        $session = FACTORY_SESSION::getInstance();
+        $message = "";
+        $fileNametxt = "";
+        
+        // Check for file input
+        if (isset($_FILES['pdf_File']))
+        {
+            $fileNamepdf = implode(DIRECTORY_SEPARATOR, [self::CACHE_DIRECTORY, \UTILS\uuid() . ".pdf"]);
+            $fileNametxt = implode(DIRECTORY_SEPARATOR, [self::CACHE_DIRECTORY, \UTILS\uuid() . ".txt"]);
+            if (move_uploaded_file($_FILES['pdf_File']['tmp_name'], $fileNamepdf))
+            {
+                $options = [];
+                
+                // Options with a numeric arg
+                foreach (["f", "l", "fixed", "linespacing", "marginl", "marginr", "margint", "marginb"] as $optname)
+                {
+                    if (isset($vars[$optname]))
+                    {
+                        $value = trim($vars[$optname]);
+                        if (is_numeric($value))
+                        {
+                            $value = intval($value);
+                            if ($value >= 0)
+                            {
+                                $options[$optname] = $value;
+                            }
+                        }
+                    }
+                }
+                
+                // Options with a string arg
+                foreach (["opw", "upw"] as $optname)
+                {
+                    if (isset($vars[$optname]))
+                    {
+                        $options[$optname] = $vars[$optname];
+                    }
+                }
+                
+                // Boolean options
+                foreach (["clip", "nodiag", "nopgbrk"] as $optname)
+                {
+                    if (isset($vars[$optname]))
+                    {
+                        $options[$optname] = "";
+                    }
+                }
+                
+                // Mode options
+                if (isset($vars["mode"]))
+                {
+                    if ($vars["mode"] != "default")
+                    {
+                        $options[$vars["mode"]] = "";
+                    }
+                }
+            
+                include_once(implode(DIRECTORY_SEPARATOR, [__DIR__, "XPDFREADER.php"]));
+                
+                $text = \XPDFREADER\pdftotext($fileNamepdf, $options);
+                file_put_contents($fileNametxt, $text);
+                @unlink($fileNamepdf);
+                
+                $message = "";
+            }
+            else
+            {
+                $message = $this->coreerrors->text("file", "upload");
+                
+            }
+        }
+        else
+        {
+            $message = $this->coreerrors->text("file", "upload");
+        }
+        
+        $this->display($message, $fileNametxt);
+    }
+    
+    /**
+     * downloadFile
+     */
+    public function downloadFile()
+    {
+        $dirName = self::CACHE_DIRECTORY;
+        $filename = $this->vars['filename'];
+        $filepath = $dirName . DIRECTORY_SEPARATOR . $filename;
+        if (file_exists($filepath))
+        {
+            $type = 'plain/text';
+            $size = filesize($filepath);
+            $lastmodified = date(DateTime::RFC1123, filemtime($filepath));
+            \FILE\setHeaders($type, $size, $filename, $lastmodified);
+            \FILE\readfile_chunked($filepath);
+        }
+        else
+        {
+            header('HTTP/1.0 404 Not Found');
+            $this->badInput->closeType = 'closePopup';
+            $this->badInput->close($this->coreerrors->text("file", "missing"));
+        }
+        die;
     }
 }
--- trunk/components/plugins/xpdftotext/xpdftotextMessages.php
+++ trunk/components/plugins/xpdftotext/xpdftotextMessages.php
@@ -19,6 +19,25 @@ class xpdftotextMessages
         $this->text = [
             "menu" => dgettext($domain, "XpdftoText"),
             "heading" => dgettext($domain, "XpdftoText tool"),
+            "convertedFileHeading" => dgettext($domain, "Converted file"),
+            "submissionFormHeading" => dgettext($domain, "Submission form"),
+            "introduction" => dgettext($domain, "This form allows you to submit a PDF file to the pdftotext tool to extract its text manually. It offers advanced options that are not available from the attachment caching feature. After a successful conversion a file containing the extracted text can be downloaded above this form."),
+            "file" => dgettext($domain, "File"),
+            "firstPage" => dgettext($domain, "First page to convert"),
+            "lastPage" => dgettext($domain, "Last page to convert"),
+            "mode" => dgettext($domain, "Mode"),
+            "characterPitch" => dgettext($domain, "Character pitch (in points)"),
+            "lineSpacing" => dgettext($domain, "Line spacing (in points)"),
+            "clip" => dgettext($domain, "Divert clipped text after covered content"),
+            "nodiag" => dgettext($domain, "No diagonal text"),
+            "nopgbrk" => dgettext($domain, "Don’t insert page breaks"),
+            "marginl" => dgettext($domain, "Margin left (in points)"),
+            "marginr" => dgettext($domain, "Margin right (in points)"),
+            "margint" => dgettext($domain, "Margin top (in points)"),
+            "marginb" => dgettext($domain, "Margin bottom (in points)"),
+            "opw" => dgettext($domain, "Owner password"),
+            "upw" => dgettext($domain, "User password"),
+            "chmodInvit" => dgettext($domain, "Some binaries of XpdftoText plugin need the executable bit. Please add it with chmod. The following commands should do the trick:"),
         ];
     }
 }
